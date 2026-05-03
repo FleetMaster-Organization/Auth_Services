97abcfd5a1e28b28439e7236848314d895e08ac3
Angy Vega <avabelcour2005@gmail.com>
2026-05-03 13:28:05 -0500

    refactor: eliminar GET /  endpoint

--- src/main/java/com/services/auth/AuthSecurityApplication.java
+++ src/main/java/com/services/auth/AuthSecurityApplication.java
@@ -4,14 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class AuthSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthSecurityApplication.class, args);
-	}
-
+	public static void main(String[] args) {SpringApplication.run(AuthSecurityApplication.class, args);}
 }
